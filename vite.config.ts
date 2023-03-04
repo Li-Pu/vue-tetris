550e17522ef8265f2c1f81ec2352bf227644a81e
李璞 <825369365@qq.com>
2023-03-04 19:36:18 +0000

    feat(package): 添加postCss支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import postcssPresetEnv from 'postcss-preset-env'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 0
+        })
+      ]
+    }
+  }
 })
